4d9543dc65e74889f8e5ae997d444c4774fe72a6
Mark <mark.hall@work.room3b.eu>
2020-03-24 21:51:02 +0000

    Improved the population stats

--- src/components/PopulationStats.vue
+++ src/components/PopulationStats.vue
@@ -24,6 +24,10 @@ export default class Home extends Vue {
         {
             text: '#',
             value: 'count',
+        },
+        {
+            text: '%',
+            value: 'percentage',
         }
     ];
     public get items() {
@@ -32,51 +36,61 @@ export default class Home extends Vue {
                 limit: 10,
                 ageBand: '< 10',
                 count: 0,
+                percentage: '-',
             },
             {
                 limit: 18,
                 ageBand: '10 - 17',
                 count: 0,
+                percentage: '-',
             },
             {
                 limit: 26,
                 ageBand: '18 - 25',
                 count: 0,
+                percentage: '-',
             },
             {
                 limit: 36,
                 ageBand: '26 - 35',
                 count: 0,
+                percentage: '-',
             },
             {
                 limit: 46,
                 ageBand: '36 - 45',
                 count: 0,
+                percentage: '-',
             },
             {
                 limit: 56,
                 ageBand: '46 - 55',
                 count: 0,
+                percentage: '-',
             },
             {
                 limit: 66,
                 ageBand: '56 - 65',
                 count: 0,
+                percentage: '-',
             },
             {
                 limit: 76,
                 ageBand: '66 - 75',
                 count: 0,
+                percentage: '-',
             },
             {
                 limit: 86,
                 ageBand: '76 - 85',
                 count: 0,
+                percentage: '-',
             },
             {
                 limit: 200,
                 ageBand: '> 85',
                 count: 0,
+                percentage: '-',
             },
         ];
         this.$store.state.population.forEach((person: Person) => {
@@ -87,6 +101,11 @@ export default class Home extends Vue {
                 }
             }
         });
+        if (this.$store.state.population.length > 0) {
+            data.forEach((entry) => {
+                entry.percentage = (entry.count / this.$store.state.population.length * 100).toFixed(2) + '%';
+            });
+        }
         return data;
     }
 }
