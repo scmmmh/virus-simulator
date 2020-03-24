bf54785bd9ab4c11e15ca0822a046a2509b32573
Mark <mark.hall@work.room3b.eu>
2020-03-24 21:42:13 +0000

    Code cleanup

--- src/webworkers/spread.ts
+++ src/webworkers/spread.ts
@@ -1,23 +1,5 @@
 import { Person } from '@/interfaces';
 
-function runSimulation(population: Person[]) {
-    let infected = [] as number[];
-    initialise(population);
-    infected.push(stats(population));
-    for (let idx = 0; idx < 50; idx++) {
-        step(population, idx);
-        infected.push(stats(population));
-    }
-    return infected;
-}
-
-function initialise(population: Person[]) {
-    for (let count = 0; count < 3; count++) {
-        const idx = Math.round(Math.random() * population.length);
-        population[idx].infected = 0;
-    }
-}
-
 export function step(population: Person[], step: number) {
     population.forEach((person) => {
         if (person.infected !== null && person.infected < step) {
@@ -31,14 +13,4 @@ export function step(population: Person[], step: number) {
         }
     });
     return population;
-}
-
-function stats(population: Person[]) {
-    return population.reduce((acc, obj) => {
-        if (obj.infected !== null) {
-            return acc + 1;
-        } else {
-            return acc;
-        }
-    }, 0);
 }
