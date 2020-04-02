fdba18fb2eed40dbe3b44d184d520f32af76cc32
Mark Hall <mark.hall@work.room3b.eu>
2020-04-02 09:52:50 +0100

    Added release information

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -2,6 +2,24 @@
     <div class="help">
         <v-container>
             <overview-text/>
+            <v-row>
+                <v-col cols="4">
+                    <v-card>
+                        <v-card-title>Release History</v-card-title>
+                        <v-list max-height="20rem" style="overflow-y: auto;">
+                            <v-subheader>02.04.2020</v-subheader>
+                            <v-list-item>
+                                <v-list-item-avatar>
+                                    <v-icon size="medium">mdi-alert-decagram</v-icon>
+                                </v-list-item-avatar>
+                                <v-list-item-content>
+                                    <v-list-item-title>Initial release of the basic simulator</v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-list>
+                    </v-card>
+                </v-col>
+            </v-row>
         </v-container>
     </div>
 </template>
